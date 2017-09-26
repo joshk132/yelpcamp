f0b4134f515ccbb0c75132a14f31e696ae643eea
Josh Kirby <jakirby@live.ca>
2017-09-26 21:47:09 +0000

    Changed port to port 80

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(80, () => {
    console.log("The YelpCamp Server Has Started!");
 });
